753f68d1242505673a5876d4924ec4dcaed8cfc6
connor <connor.sw.personal@gmail.com>
2023-06-13 12:51:14 +0000

    Additional Changes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name:str) -> None:
+def print_hi(name: str) -> None:
     """
     Parameters
     ----------
