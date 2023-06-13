f742a0d01b25b88a454d7cbd53cc93ecc4b9f8fa
connor <connor.sw.personal@gmail.com>
2023-06-13 15:38:59 +0000

    Additional Changes

--- setup.py
+++ setup.py
@@ -367,7 +367,7 @@ if output_instructions:
                             # https://github.com/ConnorAtmos/Template/blob/master/docs/toolbox/database.py#L8
 
                             documentation += f"### [{name}](/{file_document_path}#{name.lower().replace(' ', '-')}) ###\n\n"
-                            documentation += f"[{function_declaration.strip()}](./../{file_path}#L{i+1}) \n\n"
+                            documentation += f"\t[{function_declaration.strip()}](./../{file_path}#L{i+1}) \n\n"
 
                             documents = get_function_documentation(i+1)
 
